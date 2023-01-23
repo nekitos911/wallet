621e8556dbadb1fd71ccb0125dac8738c95ab1e5
nikita <n.i.k.tver@yandex.ru>
2023-01-23 21:43:37 +0300

    ResponseEntity

--- account-service/src/main/java/com/github/nekitos911/account/controller/AccountController.java
+++ account-service/src/main/java/com/github/nekitos911/account/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.github.nekitos911.account.dto.AccountRequestDto;
 import com.github.nekitos911.account.dto.AccountResponseDto;
 import com.github.nekitos911.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -13,24 +14,13 @@ public class AccountController {
     private final AccountService accountService;
 
     @GetMapping("/{accountId}")
-    public AccountResponseDto getAccount(@PathVariable Long accountId) {
-        return new AccountResponseDto(accountService.getAccountById(accountId));
+    public ResponseEntity<AccountResponseDto> getAccount(@PathVariable Long accountId) {
+        return ResponseEntity.ok(new AccountResponseDto(accountService.getAccountById(accountId)));
     }
 
     @PostMapping
-    public Long createAccount(@RequestBody AccountRequestDto accountRequestDto) {
-        return accountService.createAccount(
-                accountRequestDto.getName(),
-                accountRequestDto.getEmail(),
-                accountRequestDto.getPhone(),
-                accountRequestDto.getBills()
-        );
-    }
-
-    @PutMapping("/{accountId}")
-    public AccountResponseDto updateAccount(@PathVariable Long accountId, @RequestBody AccountRequestDto accountRequestDto) {
-        return new AccountResponseDto(accountService.updateAccount(
-                accountId,
+    public ResponseEntity<Long> createAccount(@RequestBody AccountRequestDto accountRequestDto) {
+        return ResponseEntity.ok(accountService.createAccount(
                 accountRequestDto.getName(),
                 accountRequestDto.getEmail(),
                 accountRequestDto.getPhone(),
@@ -38,8 +28,19 @@ public class AccountController {
         ));
     }
 
+    @PutMapping("/{accountId}")
+    public ResponseEntity<AccountResponseDto> updateAccount(@PathVariable Long accountId, @RequestBody AccountRequestDto accountRequestDto) {
+        return ResponseEntity.ok(new AccountResponseDto(accountService.updateAccount(
+                accountId,
+                accountRequestDto.getName(),
+                accountRequestDto.getEmail(),
+                accountRequestDto.getPhone(),
+                accountRequestDto.getBills()
+        )));
+    }
+
     @DeleteMapping("/{accountId}")
-    public AccountResponseDto deleteAccount(@PathVariable Long accountId) {
-        return new AccountResponseDto(accountService.deleteAccount(accountId));
+    public ResponseEntity<AccountResponseDto> deleteAccount(@PathVariable Long accountId) {
+        return ResponseEntity.ok(new AccountResponseDto(accountService.deleteAccount(accountId)));
     }
 }
