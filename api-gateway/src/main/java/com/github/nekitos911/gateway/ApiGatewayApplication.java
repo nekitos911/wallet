350b3c9270d7471d926ec29d4690b012f3a99135
nikita <n.i.k.tver@yandex.ru>
2023-01-25 20:14:29 +0300

    Refactoring

--- api-gateway/src/main/java/com/github/nekitos911/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/github/nekitos911/gateway/ApiGatewayApplication.java
@@ -1,13 +1,11 @@
-package com.github.nekitos911;
+package com.github.nekitos911.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
